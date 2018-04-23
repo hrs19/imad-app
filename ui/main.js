3a0b20defca8f89aab731f413f91146e1c028b44
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-23 22:47:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,28 @@ console.log('Loaded!');
 var counter  = 0;
 var button = document.getElementById('counter');
 button.onclick = function(){
-  counter = counter + 1;
+  //counter = counter + 1;
   
-  var span = document.getElementById('count');
+  //make http request
+  var request = XMLHttpRequest();
+  
+  
+  request.onreadystatechange =function(){
+    
+    if(request.readyState==XMLHttpRequest){
+        if(request.status==200){
+            
+        var counter =    request.responseText;
+        var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+        }
+        
+    }
+    
+  };
+ 
+  request.open('GET',"http://harshits1910.imad.hasura-app.io/counter",true);
+  request.send(null);
+  
+  
 };
