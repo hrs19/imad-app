a66faa2f08c80a4f8d571feba2dc782d98e3655d
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-23 16:59:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,19 +16,50 @@ app.get('/counter',function (req,res){
     
 
 
-var content = {
+var articleOne = {
+    head:'Article One|HRS|IMAD',
   title : 'Artilce One',
   date : '15th Oct,2017',
   content : `<p>This is the first article about 15th October which is not my birth date! </p>
           
           <p>This is the first article a great stair which stood in the middle of no where as a ruin!This is the first article a great stair which stood in the middle of no where as a ruin!This is the first article a great stair which stood in the middle of no where as a ruin!This is the first article a great stair which stood in the middle of no where as a ruin!</p>
-          <p>This had seen the wrath of time! </p>`
+          <p>This had seen the wrath of time! </p>`,
+          img : 'https://img00.deviantart.net/5e2d/i/2012/317/6/0/png_steps_by_paradise234-d5kvomt.png'
 };
 
+function createTemp(data){
+    var title=data.title;
+    var date=data.date;
+    var head=data.head;
+    var content=data.content;
+    var img = data.img;
 
-
-
-
+var template =`
+<!doctype html>
+<html>
+    <head>
+    <title>${title}</title>
+<link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+         <a href="http://harshits1910.imad.hasura-app.io"> HOME </a>
+        <div class="container">
+            <img src="${img}" alt="Great Stairs" height="200" width="200" class="img-medium"/>
+        </div>
+        <br>
+        <hr/>
+        <div class="container">
+            <h3>${head}</h3>
+          <div>${date}</div>
+          ${content}
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>`
+;
+return template;
+}
 
 app.get('/article-one',function(req,res){//get request made to slash then this function is executed!
    res.sendFile(path.join(__dirname,'ui','article-one.html')); 
