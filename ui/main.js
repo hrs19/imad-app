44259d7f8a1c55c926cd23a3208c1c1c5095ef61
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-23 12:15:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var image = document.getElementById('image');
+var image = document.getElementById('madi');
 image.onclick = function(){
     image.style.marginLeft = '100px';
 };
