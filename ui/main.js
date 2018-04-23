55bdae61370787e8c35843843e4694e621f550fc
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-23 22:48:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,8 @@ button.onclick = function(){
   
   request.onreadystatechange =function(){
     
-    if(request.readyState==XMLHttpRequest){
-        if(request.status==200){
+    if(request.readyState===XMLHttpRequest){
+        if(request.status===200){
             
         var counter =    request.responseText;
         var span = document.getElementById('count');
