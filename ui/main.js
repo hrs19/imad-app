509057e63a6856655ecb7a30e8f70ea2c8c0f1c3
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-24 12:19:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,17 @@ button.onclick = function(){
         var counter =    request.responseText;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
+        
+           var names = ['name1','name2','name3','name4'];
+var list = '';
+    for(var i=0;i<names.length;i++)
+    {   
+        list += '<li>'+names[i]+'</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list; 
+        
         }
         
     }
@@ -36,15 +47,7 @@ var nameInput = document.getElementById('name');
 var names = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-   var names = ['name1','name2','name3','name4'];
-var list = '';
-    for(var i=0;i<names.length;i++)
-    {   
-        list += '<li>'+names[i]+'</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list; 
+
 };
 
     
