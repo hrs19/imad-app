1847067cf1b2d2ae2801765bc65f7d5ee0e4ddab
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-24 21:43:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,30 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var config = {
-  user : 'harshits1910',
-  database:'harshits1910',
-  host: 'db.imad.hasura-app.io',
-  port:'5432',
-  password: process.env.DB_PASSWORD
-  
-};
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-   //move a select req
-   
-   //return a response with result
-   pool.query('SELECT * FROM test',function(err,result){
-      if(err){
-          res.status(500).send(err,toString());
-      }
-      else{
-          res.send(JSON.stringify(result));
-      }
-       
-   });
-});
 app.get('/', function (req, res) { //get request made to slash then this function is executed!
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -138,7 +115,30 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var config = {
+  user : 'harshits1910',
+  database:'harshits1910',
+  host: 'db.imad.hasura-app.io',
+  port:'5432',
+  password: process.env.DB_PASSWORD
+  
+};
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+   //move a select req
+   
+   //return a response with result
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err,toString());
+      }
+      else{
+          res.send(JSON.stringify(result));
+      }
+       
+   });
+});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
