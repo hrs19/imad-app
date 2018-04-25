0a9a7068628ee200961980c0c977f6a82c4af498
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-25 11:45:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/articles/:articleName',function(req,res){
            }
            else{
                var articleData =result.rows[0];
-               res.send(createTemplate(articleData));
+               res.send(createTemplate(articles[articleData]));
            }
        }
         
