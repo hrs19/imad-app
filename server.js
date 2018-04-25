83e2378e99aec318bab33ac118bcdd69dcf21740
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-25 19:17:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 function hash(input,salt){
     //how to create hash? using default crypto lib!
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ['pbkdf2','10000','salt',hashed.toString('hex')].join('$');
+    return ['pbkdf2','10000',salt,hashed.toString('hex')].join('$');
     
     
 }
