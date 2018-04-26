0a4ba9be1f8b86d7f1af18a62ec909b38c4348f2
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-26 11:28:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -98,21 +98,9 @@ submit.onclick=function(){
     
     if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
-            
-        var counter =    request.responseText;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
         
-           var names = request.responseText;
-           names  =JSON.parse(names);
-var list = '';
-    for(var i=0;i<names.length;i++)
-    {   
-        list += '<li>'+names[i]+'</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list; 
+        console.log('user logged in')    
+ 
         
         }
         
@@ -123,6 +111,8 @@ var list = '';
  var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 //var name = nameInput.value;
+console.log('username');
+console.log('password');
   request.open('POST','http://harshits1910.imad.hasura-app.io/login',true);
   request.send(JSON.stringify({username: username,password: password}));
   
