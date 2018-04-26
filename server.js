cb89d487540216ee0dec9a8d1270bc7d78d6fca3
Harshit S <36701443+hrs19@users.noreply.github.com>
2018-04-26 11:06:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ app.post('/user',function(req,res){
     var password= req.body.password;
     
     
-    var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password,salt);
+    //var salt = crypto.randomBytes(128).toString('hex');
+   //var dbString = hash(password,salt);
    pool.query('SELECT * from "user" WHERE username = $1',[username],function(err,result){
        
         if(err){
